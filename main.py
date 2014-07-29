982751d4f3da96defbe21b93240db51c75301e06
Alex Kerr <kirbyman62@gmail.com>
2014-07-29 12:52:23 +0100

    Changed to instance-based arrays for the sets
    Fixes #3

--- main.py
+++ main.py
@@ -5,13 +5,31 @@ import os
 set_objects = dict()
 
 class set: #class to hold a specific sets cards
-	rare = []
-	commoners = []
-	supers = []
-	ultra = []
-	ulti = []
-	secret = []
-	ghost = []
+	#Create the empty arrays:
+	def __init__(self):
+		self.commoners = []
+		self.rare = []
+		self.supers = []
+		self.ultra = []
+		self.ulti = []
+		self.secret = []
+		self.ghost = []
+
+	def add_card(self, rarity, cardname):
+		if(rarity == "Common"):
+			self.commoners.append(cardname)
+		if(rarity == "Rare"):
+			self.rare.append(cardname)
+		if(rarity == "Super Rare"):
+			self.supers.append(cardname)
+		if(rarity == "Ultra Rare"):
+			self.ultra.append(cardname)
+		if(rarity == "Ultimate Rare"):
+			self.ulti.append(cardname)
+		if(rarity == "Secret Rare"):
+			self.secret.append(cardname)
+		if(rarity == "Ghost Rare"):
+			self.ghost.append(cardname)
 
 	#Shuffles all the rarities in one simple call:
 	def shuffle(self):
@@ -31,47 +49,15 @@ def sort_sets():
 				set_objects[set_name]=set()
 				data = json.load(json_data)
 				for x in data:
-#					if '\n' in x['rarity'] :
-					if "hi" == "hello":
-						print "topkek"
-#						for i in x['rarity'].split('\n'):
-#							if i == "Rare":
-#								set_objects[set_name].rare.append(str(x['name']))
-#							if i == "Super Rare":
-#								set_objects[set_name].supers.append(str(x['name']))
-#							if i == "Common":
-#								set_objects[set_name].commoners.append(str(x['name']))			#if not totally fucked...
-#							if i == "Short Print":
-#								set_objects[set_name].rare.append(str(x['name']))
-#							if i == "Ultra Rare":
-#								set_objects[set_name].ultra.append(str(x['name']))
-#							if i == "Secret Rare":
-#								set_objects[set_name].secret.append(str(x['name']))
-#							if i == "Ghost Rare":
-#								set_objects[set_name].ghost.append(str(x['name']))
-#							if i == 'Ultimate Rare':
-#								set_objects[set_name].ulti.append(str(x['name']))
+					if '\n' in x['rarity'] :
+						for i in x['rarity'].split('\n'):
+							set_objects[set_name].add_card(str(x['rarity']), str(x['name']))
 					else:
-						if x['rarity'] == "Rare":
-							set_objects[set_name].rare.append(str(x['name']))
-#						if x['rarity'] == "Super Rare":
-#							set_objects[set_name].supers.append(str(x['name']))
-						if x['rarity'] == "Common":
-							set_objects[set_name].commoners.append(str(x['name']))
-#						if x['rarity'] == "Short Print":
-#							set_objects[set_name].rare.append(str(x['name']))
-#						if x['rarity'] == "Ultra Rare":
-#							set_objects[set_name].ultra.append(str(x['name']))
-#						if x['rarity'] == "Secret Rare":
-#							set_objects[set_name].secret.append(str(x['name']))
-#						if x['rarity'] == "Ghost Rare":
-#							set_objects[set_name].ghost.append(str(x['name']))
-#						if x['rarity'] == 'Ultimate Rare':
-#							set_objects[set_name].ulti.append(str(x['name']))			
+						set_objects[set_name].add_card(str(x['rarity']), str(x['name']))
+
 def generate_pack(): 
 	print set_objects.keys()
-#	selected_set_name = input('which pack?')
-	selected_set_name = "REDU-EN"
+	selected_set_name = input('which pack?')
 	selected_set = set_objects[selected_set_name]
 	pack = []
 	selected_set.shuffle()
