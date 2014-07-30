0c1e16a8ae2a6a7bf01d51f0e0cf2b08dcfbfa9d
Alex Kerr <kirbyman62@gmail.com>
2014-07-30 11:13:31 +0100

    Added HiddenArsenal.py
    I think it's best if we handle different kinds of sets as different classes, each with their own 'generate_pack()' function. I've started work on the Hidden Arsenal sets to try and get it working.

---
+++ HiddenArsenal.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+import random
+
+class Hidden_Arsenal_Like:
+	#The sets that are considered Hidden Arsenal like:
+	sets = ["HA01", "HA02", "HA03", "HA04", "HA05", "HA06", "HA07", "NUMH",
+			"DRLG"]
+
+	#Create the empty arrays:
+	def __init__(self):
+		self.supers = []
+		self.secret = []
+
+	#Adds a card of the given name to the given rarity:
+	def add_card(self, rarity, cardname):
+		if(rarity == "Super Rare"):
+			self.supers.append(cardname)
+		if(rarity == "Secret Rare"):
+			self.secret.append(cardname)
+
+	#Shuffles all the rarities in one simple call:
+	def shuffle(self):
+		random.shuffle(self.supers)
+		random.shuffle(self.secret)
+
+	#Returns a pack of the set:
+	def generate_pack(self):
+		self.shuffle()
+		pack = []
+		for i in range(0, 3):
+			pack.append(self.supers[i])
+		pack.append(self.secret[0])
+		return pack
